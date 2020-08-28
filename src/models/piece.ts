f8555377e34db8950afa05ba7f1f7f26b2d1b85d
Lee Seungmin <lee@seungmin.io>
2020-08-28 15:25:55 +0900

    feat: piece model

---
+++ src/models/piece.ts
@@ -0,0 +1,14 @@
+import { createSchema, ExtractDoc, Type, typedModel } from 'ts-mongoose';
+
+export const pieceSchema = createSchema({
+  title: Type.string({ required: true }),
+  description: Type.string({ required: true }),
+  background: Type.object().of({
+    type: Type.string({ required: true, enum: ['default', 'custom'] }),
+    path: Type.string({ required: true }),
+  }),
+  frame: Type.string({ required: true }),
+});
+
+export type pieceDoc = ExtractDoc<typeof pieceSchema>;
+export const pieceModel = typedModel('Piece', pieceSchema);
