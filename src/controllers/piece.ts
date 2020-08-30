d5bec9d4777a408a238534fde534f06c72df5bcc
Lee Seungmin <lee@seungmin.io>
2020-08-30 19:27:47 +0900

    feat: piece controller & piece routing

---
+++ src/controllers/piece.ts
@@ -0,0 +1,126 @@
+import { Request, Response, NextFunction } from 'express';
+import { pieceModel as PieceModel } from '../models/piece';
+import { exhibitionModel as ExhibitionModel } from '../models/exhibition';
+import { getUserInfoByToken } from '../resources/auth';
+import { HttpException } from '../exceptions';
+
+export default {
+  getOnePiece: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.query;
+      if (!id)
+        return next(
+          new HttpException(405, '조회할 작품 아이디와 함께 요청해주세요.'),
+        );
+      const piece = await PieceModel.findById(id);
+      if (!piece)
+        return next(
+          new HttpException(404, '정보를 조회할 작품이 존재하지 않습니다.'),
+        );
+      res.status(200).json(piece);
+    } catch (err) {
+      next(err);
+    }
+  },
+  newPiece: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const {
+        title,
+        description,
+        background,
+        frame,
+        image,
+      }: {
+        title: string;
+        description: string;
+        background: { type: 'default' | 'custom'; path: string };
+        frame: string;
+        image: string;
+      } = req.body;
+      const user = await getUserInfoByToken(String(req.token));
+      if (!user)
+        return next(new HttpException(401, '로그인 이후 이용해주세요.'));
+
+      const piece = new PieceModel({
+        title,
+        description,
+        background,
+        frame,
+        image,
+        user: user._id,
+      });
+      await piece.save();
+      res.status(200).json(piece);
+    } catch (err) {
+      next(err);
+    }
+  },
+  editPiece: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const {
+        _id,
+        title,
+        description,
+        background,
+        frame,
+        image,
+      }: {
+        _id: string;
+        title: string;
+        description: string;
+        background: { type: 'default' | 'custom'; path: string };
+        frame: string;
+        image: string;
+      } = req.body;
+      const user = await getUserInfoByToken(String(req.token));
+      if (!user)
+        return next(new HttpException(401, '로그인 이후 이용해주세요.'));
+
+      const piece = await PieceModel.findById(_id);
+      if (!piece)
+        return next(
+          new HttpException(404, '정보를 변경할 작품이 존재하지 않습니다.'),
+        );
+      if (piece.user.toString() !== user._id.toString())
+        return next(new HttpException(403, '권한이 없습니다.'));
+      await piece.updateOne({
+        title,
+        description,
+        background,
+        frame,
+        image,
+      });
+      res.status(200).json(piece);
+    } catch (err) {
+      next(err);
+    }
+  },
+  connectPiecesToExhibition: async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const {
+        exhibitionId,
+        pieces,
+      }: { exhibitionId: string; pieces: Array<string> } = req.body;
+      const user = await getUserInfoByToken(String(req.token));
+      if (!user)
+        return next(new HttpException(401, '로그인 이후 이용해주세요.'));
+      const exhibition = await ExhibitionModel.findById(exhibitionId);
+      if (!exhibition)
+        return next(
+          new HttpException(404, '정보를 변경할 전시회가 존재하지 않습니다.'),
+        );
+      if (exhibition.user.toString() !== user._id.toString())
+        return next(new HttpException(403, '권한이 없습니다.'));
+      await exhibition.updateOne({
+        pieces,
+      });
+      res.status(200).json(exhibition);
+    } catch (err) {
+      next(err);
+    }
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,8 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import {
-  addExhibition,
-  editExhibition,
-  getExhibition,
-} from '../resources/exhibition';
-import {
-  editPiece,
-  addPiece,
-  getPiece,
-  connectPiecesToExhibition,
-} from '../resources/piece';
+import { Router, Response } from 'express';
+
 import AuthController from '../controllers/auth';
 import ExhibitionController from '../controllers/exhibition';
+import PieceController from '../controllers/piece';
 
 const router = Router();
 
@@ -33,63 +24,12 @@ router.post('/exhibition/new', ExhibitionController.newExhibition);
 
 router.post('/exhibition/edit', ExhibitionController.editExhibition);
 
-router.get('/piece', async (req: Request, res: Response) => {
-  try {
-    const { id } = req.query;
-    const piece = await getPiece(String(id));
-    res.status(200).json(piece);
-  } catch ({ message }) {
-    res.status(500).json({ message });
-  }
-});
+router.get('/piece', PieceController.getOnePiece);
 
-router.post('/piece/new', async (req, res) => {
-  try {
-    const { title, description, background, frame, image } = req.body;
-    const piece = await addPiece(
-      title,
-      description,
-      background,
-      frame,
-      image,
-      String(req.token),
-    );
-    res.status(200).json(piece);
-  } catch ({ message }) {
-    res.status(500).json({ message });
-  }
-});
+router.post('/piece/new', PieceController.newPiece);
 
-router.post('/piece/edit', async (req, res) => {
-  try {
-    const { _id, title, description, background, frame, image } = req.body;
-    const piece = await editPiece(
-      _id,
-      title,
-      description,
-      background,
-      frame,
-      image,
-      String(req.token),
-    );
-    res.status(200).json(piece);
-  } catch ({ message }) {
-    res.status(500).json({ message });
-  }
-});
+router.post('/piece/edit', PieceController.editPiece);
 
-router.post('/piece/connect', async (req, res) => {
-  try {
-    const { exhibitionId, pieces } = req.body;
-    const exhibition = await connectPiecesToExhibition(
-      exhibitionId,
-      pieces,
-      String(req.token),
-    );
-    res.status(200).json(exhibition);
-  } catch ({ message }) {
-    res.status(500).json({ message });
-  }
-});
+router.post('/piece/connect', PieceController.connectPiecesToExhibition);
 
 export default router;
